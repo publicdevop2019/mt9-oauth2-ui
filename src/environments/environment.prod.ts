23f4e138376700bf04b1e5c85ddea97eba45c26d
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-04 23:22:38 -0400

    add port number

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   mode: 'online' as 'online' | 'offline',
   home: 'https://github.com/publicdevop2019/oauth2service',
-  serverUri: 'http://api.manytreetechnology.com',
-  tokenUrl: 'http://api.manytreetechnology.com/auth-svc/oauth/token',
+  serverUri: 'http://api.manytreetechnology.com:8111',
+  tokenUrl: 'http://api.manytreetechnology.com:8111/auth-svc/oauth/token',
   loginClientId: 'login-id',
   registerClientId: 'register-id',
   clientSecret: ''
